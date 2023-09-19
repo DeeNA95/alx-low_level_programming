b18e8e5a7a4301ae5d27f7849032c11724c17760
Derek Amuna <dadadee02@outlook.com>
2023-09-19 01:36:03 +0000

    kl

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints first n elements of an array
+ * @a: int array to display from
+ * @n: number of elements to display
+ *
+ * Retuen: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == n - 1)
+			printf("%d", *(a + i));
+		else
+			printf("%d, ", *(a + i));
+		i++;
+	}
+	printf("\n");
+}

6c27d59998142cf5c11eb0d801e14d9aa5942794
Derek Amuna <dadadee02@outlook.com>
2023-12-08 05:14:56 +0000

    linked lisys

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a node at the end of a double linked list
+ * @head: pointer to the first node
+ * @n: number to fill the node
+ * Return: address of the new element / NULL if failed
+ **/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+
+
+	dlistint_t *temp, *new_node;
+
+	temp = *head;
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (!*head)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	new_node->prev = temp;
+	temp->next = new_node;
+	return (new_node);
+}
